e6f8df6925a711202e6170ca1bf181bbc0226f1b
Pablo <phuete@protonmail.com>
2022-09-01 23:26:15 +0200

    Trabajo Final 1-3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //ESTIM - Pagina de venta de videojuegos
+USUARIOLOGADO = false
 
 const CATEGORIAS = ['Shooter', 'RPG', 'Deportes', 'Terror', 'Indie']
 
@@ -18,57 +19,26 @@ const CARRITO = []
 
 const IVA = 1.21
 
-let USUARIOLOGADO = false
-
 const modalContainer = document.getElementById('modal_container');
 const openCart = document.getElementById('open');
 const closeCart = document.getElementById('close');
 const modalCart = document.getElementById('modalCart');
 
-openCart.addEventListener('click', ()=>{
+openCart.addEventListener('click', () => {
     modalContainer.classList.add('modal-active')
 })
 
-closeCart.addEventListener('click', ()=>{
+closeCart.addEventListener('click', () => {
     modalContainer.classList.remove('modal-active')
 })
 
- modalContainer.addEventListener('click', ()=>{
+modalContainer.addEventListener('click', () => {
     closeCart
- })
+})
 
-modalCart.addEventListener('click', (e)=>{
+modalCart.addEventListener('click', (e) => {
     e.stopPropagation
- })
-
-
-class Videojuego {
-    constructor(id, nombre, precio, categoria, stock, img) {
-        id = parseInt(Math.random() * 100000)
-        this.id = id    
-        this.nombre = nombre
-        this.precio = precio
-        this.categoria = categoria
-        this.stock = stock
-        this.img = img
-    }
-    
-}
-
-
-class Usuario {
-    constructor(nombre, apellido, nombreUsuario, tipo) {
-        this.id = parseInt(Math.random() * 100000)
-        this.nombre = nombre
-        this.apellido = apellido
-        this.nombreUsuario = nombreUsuario
-        this.tipo = tipo
-        //this.biblioteca = []
-        //this.promociones = CODIGOSPROMO
-    }
-}
-
-
+})
 
 generadorAutomaticoJuegos()
 console.table(VIDEOJUEGOS)
@@ -82,7 +52,7 @@ function generadorAutomaticoJuegos() {
     VIDEOJUEGOS.push(new Videojuego(2345, "Elden Ring", 69.95, "RPG", 7, 'src/img/ELDENRING.jpg'))
     VIDEOJUEGOS.push(new Videojuego(3456, "NBA2k23", 44.99, "Deportes", 19, 'src/img/NBA2K23.jpg'))
     VIDEOJUEGOS.push(new Videojuego(4567, "Madison", 50.00, "Terror", 3, 'src/img/MADISON.jpg'))
-    VIDEOJUEGOS.push(new Videojuego(4567, "Stardew Valley", 18.99, "Indie", 12, 'src/img/STARDEWVALLEY.jpg')) 
+    VIDEOJUEGOS.push(new Videojuego(4567, "Stardew Valley", 18.99, "Indie", 12, 'src/img/STARDEWVALLEY.jpg'))
 }
 
 function generadorAutomaticoUsuarios() {
@@ -90,25 +60,38 @@ function generadorAutomaticoUsuarios() {
     USUARIOS.push(new Usuario("Margarita", "Rodriguez", "Marga89", TIPOUSUARIO[1]))
 }
 
+
+
 function login() {
     let usuarioExiste = false
 
     while (!usuarioExiste) {
         let nombreUsuario = prompt("Bienvenido a ESTIM. Introduce un nombre de usuario ")
-        if (nombreUsuario === null){return}
+        if (nombreUsuario === null) { return }
         USUARIOS.forEach(usuario => {
             if (usuario.nombreUsuario === nombreUsuario) {
-                usuarioExiste = true
+                usuarioExiste = true                
             }
+            
         });
         if (!usuarioExiste) {
-            alert("No existe ningun usuario con el nombre " + nombreUsuario + ". Vuelve a la pagina de inicio y registrate en 'UNETE AQUI'")
-          }
-        
+            Swal.fire({
+                title: 'Error!',
+                text: "No existe ningun usuario con el nombre " + nombreUsuario + ". Vuelve a la pagina de inicio y registrate en 'UNETE AQUI'",
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })
+        }
+
         else {
             prompt("Introduzca la contraseña:")
-            alert("Bienvenido, '" + nombreUsuario + "'")
-            USUARIOLOGADO = true
+            swal.fire({
+                title: 'Bienvenido ' + nombreUsuario + '!', 
+                text: 'Has iniciado sesion correctamente',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            })
+            USUARIOLOGADO = true        
         }
     }
 
@@ -118,91 +101,146 @@ function login() {
 
 function registroUsuario() {
     let cancelar = false
-    while (!cancelar){
+    while (!cancelar) {
         let nombreUsuario = prompt("Introduce tu nombre de usuario:")
-        if (nombreUsuario === null){
-            alert("Vuelve para registrarte cuando quieras")
+        if (nombreUsuario === null) {
+            Swal.fire({
+                title: '¿Ya te vas?',
+                text: "Aun no has terminado tu registro. Vuelve pronto a registrarte!",
+                icon: 'info',
+                confirmButtonText: 'Adios!'
+              })
             break
         }
         while (nombreUsuario === "") {
             nombreUsuario = prompt("Introduce un nombre de usuario valido")
         }
         let contrasenya = prompt("Introduce una contraseña")
-        if (contrasenya === null){
-            alert("Vuelve para registrarte cuando quieras")
-            break
+        if (contrasenya === null) {
+            Swal.fire({
+                title: '¿Ya te vas?',
+                text: "Aun no has terminado tu registro. Vuelve pronto a registrarte!",
+                icon: 'info',
+                confirmButtonText: 'Adios!'
+              })
+              break
         }
         while (contrasenya === "" || contrasenya === null) {
             contrasenya = prompt("Introduce una contraseña valida")
         }
         let nombre = prompt("Introduce tu nombre:")
-        if (nombre === null){
-            alert("Vuelve para registrarte cuando quieras")
+        if (nombre === null) {
+            Swal.fire({
+                title: '¿Ya te vas?',
+                text: "Aun no has terminado tu registro. Vuelve pronto a registrarte!",
+                icon: 'info',
+                confirmButtonText: 'Adios!'
+              })
             break
         }
         while (nombre === "") {
             nombre = prompt("Introduce un nombre valido")
         }
         let apellido = prompt("Introduce tu apellido:")
-        if (apellido === null){
-            alert("Vuelve para registrarte cuando quieras")
+        if (apellido === null) {
+            Swal.fire({
+                title: '¿Ya te vas?',
+                text: "Aun no has terminado tu registro. Vuelve pronto a registrarte!",
+                icon: 'info',
+                confirmButtonText: 'Adios!'
+              })
             break
         }
         while (apellido === "") {
             apellido = prompt("Introduce un apellido valido")
         }
         let tipo = prompt("¿Que tipo de plan deseas adquirir? (BASICO o PREMIUM)")
-        if (tipo === null){
-            alert("Vuelve para registrarte cuando quieras")
+        if (tipo === null) {
+            Swal.fire({
+                title: '¿Ya te vas?',
+                text: "Aun no has terminado tu registro. Vuelve pronto a registrarte!",
+                icon: 'info',
+                confirmButtonText: 'Adios!'
+              })
             break
         }
         while (tipo === "") {
             tipo = prompt("Introduce un plan valido (BASICO o PREMIUM")
         }
-        alert("Usuario '" + nombreUsuario + "' ha sido creado con éxito")
+        Swal.fire({
+            title: '¡Enhorabuena!',
+            text: "Usuario '" + nombreUsuario + "' ha sido creado con éxito",
+            icon: 'success',
+            confirmButtonText: 'Continuar'
+          })
         USUARIOS.push(new Usuario(nombre, apellido, nombreUsuario, tipo))
-        cancelar=true
+        cancelar = true
     }
-   
+
 }
 
-const mostrarVideojuegos = (VIDEOJUEGOS) =>{
+const mostrarVideojuegos = (VIDEOJUEGOS) => {
     const productsContainer = document.getElementById('product-container');
-    VIDEOJUEGOS.forEach(videojuego=>{
+    VIDEOJUEGOS.forEach(videojuego => {
         const card = document.createElement('card');
         card.innerHTML += `<div class="card" style="width:16rem;">
                                  <img src="${videojuego.img}" class="card-img-top" alt="...">
                                  <div class="card-body">
                                     <h5 class="card-title">${videojuego.nombre}</h5>
                                      <p class="card-text">Precio:€ ${videojuego.precio}</p>
-                                     <p id="cantidad" class="card-text">Stock: ${videojuego.stock}</p>
+                                     <p id="cantidad${videojuego.id}" class="card-text">Stock: ${videojuego.stock}</p>
                                     <button class="btn-primary" id="button${videojuego.id}">Comprar</button>
                             </div>
                          </div>`
 
-    productsContainer.appendChild(card);
-    const button = document.getElementById(`button${videojuego.id}`)
-    button.addEventListener('click', ()=>{
-    cart(videojuego.id)
-    if (!USUARIOLOGADO) {
-               alert("Inicia sesión antes de continuar")
-               return}
-        if(videojuego.stock > 0){
-            videojuego.stock--; alert(`Has añadido ${videojuego.nombre}`)}
-        else{
-            alert("No hay stock disponible")}
-        ;      
+        productsContainer.appendChild(card);
+
+        let button = document.getElementById(`button${videojuego.id}`)
+
+        button.addEventListener('click', () => {
+            if (!USUARIOLOGADO) {  
+                Swal.fire({
+                    title: 'Error!',
+                    text: "Debes iniciar sesion para poder comprar",
+                    icon: 'error',
+                    confirmButtonText: 'Volver'
+                    })    
+                    return
+
+            } else {
+                if (videojuego.stock > 0) {
+                    addToCart(videojuego.id)
+                    videojuego.stock--
+                    document.getElementById(`cantidad${videojuego.id}`).innerHTML = `Stock: ${videojuego.stock}`
+
+                    Swal.fire({
+                        title: '¡Enhorabuena!',
+                        text: "Has añadido al carrito " + videojuego.nombre + " con exito",   
+                        icon: 'success',                                    
+                        confirmButtonText: 'Continuar'
+                        })
+                }
+                else {
+                    swal.fire({
+                        title: 'Lo sentimos!',
+                        text: "No quedan unidades de " + videojuego.nombre + " en stock",
+                        icon: 'error',
+                        confirmButtonText: 'Volver'
+                        })
+               }
+            }
+            
+        })
     })
-    })
-}  
+}
 mostrarVideojuegos(VIDEOJUEGOS)
 
-const cart = (videojuegoId) =>{
-    const cartContainer = document.getElementById('cart-container');
+const addToCart = (videojuegoId) => {
+    const cartContainer = document.getElementById('cart-container')
     const mostrarEnCarrito = () => {
-        let videojuego = VIDEOJUEGOS.find(videojuego => videojuego.id == videojuegoId);
+        let videojuego = VIDEOJUEGOS.find(videojuego => videojuego.id == videojuegoId)
         CARRITO.push(videojuego);
-        localStorage.setItem("products", JSON.stringify(CARRITO));
+        localStorage.setItem("products", JSON.stringify(CARRITO))
         let div = document.createElement('div');
         div.classList.add('productInCart');
         div.innerHTML = `<p>${videojuego.nombre}</p>
@@ -213,18 +251,19 @@ const cart = (videojuegoId) =>{
         cartContainer.appendChild(div);
         let butttonDelete = document.getElementById(`delete${videojuego.id}`);
         butttonDelete.addEventListener('click', eliminarVideojuego);
-        
-    
-      }
-    
-      mostrarEnCarrito();
+
+
     }
-    
-   
-    function eliminarVideojuego(e){
-        let btnClicked = e.target;
-        btnClicked.parentElement.remove();
-    }
+
+    mostrarEnCarrito();
+}
+
+
+function eliminarVideojuego(e) {
+    let btnClicked = e.target;
+    btnClicked.parentElement.remove();
+    VIDEOJUEGOS.videojuego.stock++;
+}
 
 
 let iniciarSesionBtn = document.getElementById("iniciarSesionBtn")
@@ -234,12 +273,17 @@ let registroUsuarioBtn = document.getElementById("registroUsuarioBtn")
 registroUsuarioBtn.onclick = () => registroUsuario()
 
 let CarritoCompraBtn = document.getElementById("comprarCarrito")
-registroUsuarioBtn.onclick = () => comprar() 
+CarritoCompraBtn.onclick = () => comprar()
 
 function comprar() {
     let total = 0
     for (let i = 0; i < CARRITO.length; i++) {
         total += CARRITO[i].precio
     }
-    alert("Has realizado la compra con exito.El total es " + total + "€")
+    swal.fire({
+        title: '¡Enhorabuena!',
+        // text: "Has comprado " + CARRITO.map((videojuego) =>{return {videojuego.nombre}) + " por un total de " + total + "€",
+        icon: 'success',
+        confirmButtonText: 'Continuar'
+        })
 }
